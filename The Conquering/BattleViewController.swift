86e625aeddbaa1300fe3b7d57b67556411dcd6b7
Jake Buller <jake.buller@enflick.com>
2016-11-26 19:54:54 -0500

    animate dice roll

--- The Conquering/BattleViewController.swift
+++ The Conquering/BattleViewController.swift
@@ -8,17 +8,28 @@
 
 import UIKit
 
-class BattleViewController: UIViewController {
+class BattleViewController: UIViewController, CAAnimationDelegate {
 
     @IBOutlet var rollButton: UIButton!
     @IBOutlet var diceImage: UIImageView!
+    @IBOutlet var diceContainerView: UIView!
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         // UI Styling
         rollButton.layer.cornerRadius = 8
-        diceImage.layer.cornerRadius = 8
+        diceContainerView.layer.cornerRadius = 8
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,20 +44,27 @@ class BattleViewController: UIViewController {
     }
 
     func rollDice() {
+        rotate360Degrees(view: diceImage, duration: 0.4, completionDelegate: self)
+    }
+    
+    func rotate360Degrees(view: UIImageView, duration: CFTimeInterval = 1.0, completionDelegate: CAAnimationDelegate? = nil) {
+        let rotateAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        rotateAnimation.fromValue = 0.0
+        rotateAnimation.toValue = CGFloat(M_PI * 2.0)
+        rotateAnimation.duration = duration
+        
+        if let delegate: CAAnimationDelegate = completionDelegate {
+            rotateAnimation.delegate = delegate
+        }
+        
+        view.layer.add(rotateAnimation, forKey: nil)
+    }
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        NSLog("Animation stopped")
         let diceVal = arc4random_uniform(6) + 1
         NSLog("Rolled: " + String(diceVal))
         let imageName = "dice-" + String(diceVal)
         diceImage.image = UIImage(named: imageName)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
