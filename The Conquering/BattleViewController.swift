6440e57a171118394f982ce1f879d4fa5860484b
Jake Buller <jake.buller@enflick.com>
2016-12-06 17:55:33 -0500

    animated dice with programtic view creation

--- The Conquering/BattleViewController.swift
+++ The Conquering/BattleViewController.swift
@@ -12,7 +12,13 @@ class BattleViewController: UIViewController, CAAnimationDelegate {
 
     @IBOutlet var rollButton: UIButton!
     @IBOutlet var diceImage: UIImageView!
-    @IBOutlet var diceContainerView: UIView!
+    
+    
+    @IBOutlet var attackDieOne: UIImageView!
+    @IBOutlet var attackDieTwo: UIImageView!
+    @IBOutlet var attackDieThree: UIImageView!
+    
+    var attackDice = [DiceView]()
     
     /*
      // MARK: - Navigation
@@ -27,9 +33,41 @@ class BattleViewController: UIViewController, CAAnimationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        
+        // TODO: This needs to be updated to properly add constraints and 
+        // display UI dynamically
+        
         // UI Styling
         rollButton.layer.cornerRadius = 8
-        diceContainerView.layer.cornerRadius = 8
+        
+        // Create a stack view to house our dice views
+        let attackDiceView = UIStackView()
+        
+        // Create a new dice view
+        let diceView1 = DiceView()
+        diceView1.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        diceView1.backgroundColor = UIColor.red
+        diceView1.layer.cornerRadius = 8
+        
+        // Create a new dice view
+        let diceView2 = DiceView()
+        diceView2.frame = CGRect(x: 110, y: 0, width: 100, height: 100)
+        diceView2.backgroundColor = UIColor.red
+        diceView2.layer.cornerRadius = 8
+        
+        // Create a new dice view
+        let diceView3 = DiceView()
+        diceView3.frame = CGRect(x: 220, y: 0, width: 100, height: 100)
+        diceView3.backgroundColor = UIColor.red
+        diceView3.layer.cornerRadius = 8
+        
+        attackDiceView.addSubview(diceView1)
+        attackDiceView.addSubview(diceView2)
+        attackDiceView.addSubview(diceView3)
+        attackDiceView.spacing = 4
+        self.view.addSubview(attackDiceView)
+        
+        attackDice = [diceView1, diceView2, diceView3]
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,32 +77,13 @@ class BattleViewController: UIViewController, CAAnimationDelegate {
     
     
     @IBAction func rollClicked(_ sender: Any) {
-        NSLog("roll clicked")
-        rollDice()
+        for die in attackDice {
+            die.roll()
+        }
+
     }
 
-    func rollDice() {
-        rotate360Degrees(view: diceImage, duration: 0.4, completionDelegate: self)
-    }
-    
-    func rotate360Degrees(view: UIImageView, duration: CFTimeInterval = 1.0, completionDelegate: CAAnimationDelegate? = nil) {
-        let rotateAnimation = CABasicAnimation(keyPath: "transform.rotation")
-        rotateAnimation.fromValue = 0.0
-        rotateAnimation.toValue = CGFloat(M_PI * 2.0)
-        rotateAnimation.duration = duration
+    func doRoll(die: UIImageView) {
         
-        if let delegate: CAAnimationDelegate = completionDelegate {
-            rotateAnimation.delegate = delegate
-        }
-        
-        view.layer.add(rotateAnimation, forKey: nil)
-    }
-    
-    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
-        NSLog("Animation stopped")
-        let diceVal = arc4random_uniform(6) + 1
-        NSLog("Rolled: " + String(diceVal))
-        let imageName = "dice-" + String(diceVal)
-        diceImage.image = UIImage(named: imageName)
     }
 }
--- The Conquering/DiceView.swift
+++ The Conquering/DiceView.swift
@@ -6,4 +6,53 @@
 //  Copyright © 2016 Jake Buller. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class DiceView: UIView, CAAnimationDelegate {
+    
+    var value : Int = 0
+    
+    var imageView : UIImageView? = nil
+    
+    override init (frame : CGRect) {
+        super.init(frame : frame)
+        addBehavior()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func roll() {
+        // Update the current value
+        self.imageView!.image = UIImage(named: "dice-random")
+        self.value = Int(arc4random_uniform(6)) + 1
+        self.rotate360Degrees(duration: 0.5)
+    }
+    
+    private func addBehavior (){
+        imageView = UIImageView(image: UIImage(named: "dice-random"))
+        imageView!.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        self.addSubview(imageView!)
+    }
+    
+    private func rotate360Degrees(duration: CFTimeInterval = 1.0) {
+        let rotateAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        rotateAnimation.fromValue = 0.0
+        rotateAnimation.toValue = CGFloat(M_PI * 2.0)
+        rotateAnimation.duration = duration
+        rotateAnimation.delegate = self
+        
+        self.imageView!.layer.add(rotateAnimation, forKey: nil)
+    }
+    
+    private func updateImage() {
+        let imageName = "dice-" + String(value)
+        self.imageView!.image = UIImage(named: imageName)
+    }
+    
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        self.updateImage()
+    }
+}
+
